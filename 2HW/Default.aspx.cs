9669aaa432ce052f4a0b27e592e50aa5b9b8e4b8
昱瑋 李 <107111226@mail.oit.edu.tw>
2021-03-30 13:53:00 +0800

    2

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,7 +8,15 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int num = 10, sum = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Response.Write(sum);
         }
     }
 }
